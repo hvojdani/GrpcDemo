e06cea0911b0dbfcda1b9c305dca7c7dcacadecc
Hamed Vojdani <hamed-vojdani@hotmail.com>
2021-05-27 11:42:57 +0430

    Add project files.

---
+++ WebAppClient/Controllers/GrpcCallController.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Client.GrpcEndPoint;
+
+namespace WebAppClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GrpcCallController : ControllerBase
+    {
+        private readonly Greeter.GreeterClient _client;
+        public GrpcCallController(Greeter.GreeterClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<string> Get(string name)
+        {
+            var reply = await _client.SayHelloAsync(
+                              new HelloRequest { Name = name });
+
+            return reply.Message;
+        }
+    }
+}
---
+++ WebAppServer/GrpcServices/GreeterService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppServer.GrpcEndPoint;
+
+namespace WebAppServer.GrpcServices
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply() { Message = $"Hello {request.Name }" });
+        }
+    }
+}
